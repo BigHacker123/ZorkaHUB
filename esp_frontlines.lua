56d4fef841e8b92d88282ea2c77a2ae469208cd6
ZuhuInc <66175704+ZuhuInc@users.noreply.github.com>
2023-02-26 22:15:42 +0100

    Update esp_frontlines.lua

--- esp_frontlines.lua
+++ esp_frontlines.lua
@@ -1,7 +1,7 @@
 _G.ESP = {
     Boxes = true,
     Tracers = true,
-    Rainbow = true,
+    Rainbow = false,
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mopsfl/roblox-scripts/main/.frontlines-esp.lua"))()
